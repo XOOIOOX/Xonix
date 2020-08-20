39434e863e3d0c782e221a0d66faf89f6bb6be5c
TRANTOR <trantor8@gmail.com>
2020-08-20 16:54:50 +0300

    Typo

--- CentralDataStruct.h
+++ CentralDataStruct.h
@@ -7,7 +7,7 @@ struct CentralDataStruct
 	MatrixCells level{ LevelWidth, LevelHeigth, CellType::Water };							// матрица уровня
 	QGraphicsScene* scene;																	// сцена для отрисовки
 	MonsterList monsterList;																// список монстров
-	TrackList trackList;																	// спиское ячеек "трека"
+	TrackList trackList;																	// список ячеек "трека"
 
-	CellType& cellAccess(const QPoint& point);												// доступ к чейке уровня
+	CellType& cellAccess(const QPoint& point);												// доступ к ячейке уровня
 };
